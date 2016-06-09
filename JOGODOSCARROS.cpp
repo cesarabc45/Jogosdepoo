ceb3d5176320e8fe03963a800b2d373b355b5600
cesarabc45 <juliocesarabc45@gmail.com>
2016-06-08 23:26:50 -0300

    Update JOGODOSCARROS.cpp

--- JOGODOSCARROS.cpp
+++ JOGODOSCARROS.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
-#include <ctime>
-using namespace std;
+#include <iostream> //abrindo à biblioteca
+#include <ctime> //abrindo outra biblioteca para gerar espaços aleatório
+using namespace std; 
 
-void imprimir_espacos(int rodada);
-int main()
+void imprimir_espacos(int rodada); //declarando a função que imprime os espaços
+int main() //função principal
 {
-	int total_espacos = 50;
-	int espacos_1 = 0;
-	int espacos_2 = 0;
-	while (espacos_1 < total_espacos && espacos_2 < total_espacos)
+	int total_espacos = 50; //declarando à quantidade de espaços necessário para chegar na linha de chegada 
+	int espacos_pjd = 0; //declarando à quantidade de espaços que o carro pjd percorreu
+	int espacos_profmarcelo = 0; //declarando à quantidade de espaços que o carro profmarcelo percorreu
+	while (espacos_pjd < total_espacos && espacos_profmarcelo < total_espacos) //
 	{
-		system("cls");
-		cout << "Jogo do carrinho de corrida legal" << endl;
-		cout << "                                                Chegada!" << endl;
+		system("cls"); //limpando a tela
+		cout << "Jogo do carro de corrida" << endl; //imprimindo na tela o nome do jogo
+		cout << "                                                Chegada!" << endl; //imprimindo a chegada
 
-		srand((int)time(0));
+		srand((int)time(0)); //declarando a função que imprime números aleatórios
 
-		espacos_1 = rand() % 3 + 1 + espacos_1;
-		imprimir_espacos(espacos_1);
+		espacos_pjd = rand() % 3 + 1 + espacos_pjd; //gerando um número aleatório de 1 a 3 somando mais do resultado, para o carro pjd andar
+		imprimir_espacos(espacos_pjd); //imprimindo os espaços
 		cout << "  _  " << endl;
-		imprimir_espacos(espacos_1);
+		imprimir_espacos(espacos_pjd);
 		cout << "-o-o>" << endl;
 
-		espacos_2 = rand() % 3 + 1 + espacos_2;
-		imprimir_espacos(espacos_2);
+		espacos_profmarcelo = rand() % 3 + 1 + espacos_profmarcelo;
+		imprimir_espacos(espacos_profmarcelo);
 		cout << "  _  " << endl;
-		imprimir_espacos(espacos_2);
+		imprimir_espacos(espacos_profmarcelo);
 		cout << "-o-o>" << endl;
 	}
-	if (espacos_1 > espacos_2) cout << "Carro 1 venceu a corrida!" << endl;
-	else if (espacos_2 > espacos_1) cout << "Carro 2 venceu a corrida!" << endl;
+	if (espacos_pjd > espacos_profmarcelo) cout << "Carro pjd venceu a corrida!" << endl;
+	else if (espacos_profmarcelo > espacos_pjd) cout << "Carro profmarcelo venceu a corrida!" << endl;
 	else cout << "Empate!" << endl;
 	return 0;
 }
